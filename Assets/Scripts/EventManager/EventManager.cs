fd70aaa3d14bbc4c1412245d7aef2971533b0833
liuceyi <602483575@qq.com>
2020-06-27 16:58:26 +0800

    add eventManager（beta）

---
+++ Assets/Scripts/EventManager/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EventManager
+{
+    static EventManager() 
+    {
+        if (eventManager == null) 
+        {
+            eventManager = new EventManager();
+        }
+
+    }
+    private static EventManager eventManager;
+    private Dictionary<EventType, object> eventDic = new Dictionary<EventType, object>();
+}
